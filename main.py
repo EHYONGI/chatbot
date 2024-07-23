34dc603a4f814034c94a92d602692443b983374a
EHYONGI <irene4618@naver.com>
2024-07-23 17:02:34 +0900

    챗봇 만들기~ 대답까지

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import requests
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+
+from utils import random_number
+from utils import kospi, openai, langchain
+
+load_dotenv()
+
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+URL = f'https://api.telegram.org/bot{TOKEN}'
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+
+app = FastAPI()
+
+
+@app.post("/")
+async def read_root(request: Request):
+
+    body = await request.json()
+    
+    user_id = body['message']['from']['id']
+    user_input = body['message']['text']
+
+    if user_input[0] == '/':
+        # 우리가 만든 기능 추가
+        if user_input == '/lotto':
+            text = random_number()
+            print(text)
+        elif user_input == '/kospi':
+            text = kospi()
+        else:
+            text = '지원하지 않는 기능입니다.'
+
+    else:
+        # openAI API 활용
+        text = langchain(OPENAI_API_KEY, user_input)
+        #text = '지원하는 기능은 /lotto입니다.'
+
+    req_url = f'{URL}/sendMessage?chat_id={user_id}&text={text}'
+    
+    requests.get(req_url)
+
+    return {"Hello": "World"}
